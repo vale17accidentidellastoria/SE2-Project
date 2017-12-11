0d7e11df7888240b55d81b71392d4fa14450d44b
zanzibar184 <gianluca.pessotto96@gmail.com>
2017-12-11 19:50:03 +0100

    implementazione risposta del santo dal chatbot

--- src/components/ChatBot/ChatBot.js
+++ src/components/ChatBot/ChatBot.js
@@ -22,6 +22,8 @@ class ChatBot extends React.Component {
 
         // Bind del metodo sendInput per poterlo utilizzare all'interno del metodo render()
         this.sendInput = this.sendInput.bind(this);
+        this.showSaint = this.showSaint.bind(this);
+
 
     }
 
@@ -167,11 +169,13 @@ class ChatBot extends React.Component {
                             messaggio = <p>Potresti iniziare chiedendogli di suo/a marito/moglie per poi parlare dei figli e a loro volta se si sono sposati e se hanno avuto figli. <br/> Per ogni persona si può chiedere se ha qualche aneddoto o cosa gli/le piace/piaceva fare insieme.</p>
                         }else if(response.result.action === "santoDelGiorno")
                         {
-                            /*let data = new Date();
-                            let datastring = data.getDate() + "/" + data.getMonth() + 1 + "/" + data.getFullYear();*/
+                            let data = new Date();
+                            let datastring = data.getDate() + "/" + data.getMonth() + 1 + "/" + data.getFullYear();
+                            // --------------------------------------------------------------------------------------
 
-                            let santo = "S. Domenico";
-                            messaggio= <p>Il santo del giorno e' {santo}</p>
+                            this.showSaint(data.getDay(),data.getMonth());
+                            console.log("giorno " + data.getDay() + " mese" + data.getMonth())
+                            messaggio= <p>Il santo del giorno e': </p>
                         }
 
                     if(item.speech)// se non vuoto mostro il testo di quel messaggio aggiungendolo a messageList
@@ -190,6 +194,22 @@ class ChatBot extends React.Component {
             .catch((error) => { console.log("ChatBot Error: " + error);/* do something here too */}) // Se c'è un errore lo riporto
             .then(() => input.disabled = false)  // Infine: abilito nuovamente l'input per permettere all'utente di scrivere un nuovo messaggio
     }
+
+    //prende il santo dal db e crea il messaggio di risposta
+    showSaint(day, month) {
+
+
+        session.getSaintContent(day, month, (results)=> {
+            results.forEach((element) => {
+                this.messageList.addComponent(<a className='list-group-item Msj_server'><b><p>{element.santo}</p></b></a>);
+            })
+        });
+
+
+    }
+
+
+
 
     // Metodo chiamato durante il render del componente ( lista dei messaggi + input utente )
     render() {
